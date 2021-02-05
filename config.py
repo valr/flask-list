307fd10e23f6d08b0219bc293b60c060579c151b
Valère Monseur <valere.monseur@ymail.com>
2021-02-05 21:49:39 +0100

    set the mail port to 8025 (usable by everybody)

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     # SQLALCHEMY_ECHO = True
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
