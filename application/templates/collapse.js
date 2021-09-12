a3f9bc3029cd9449bd23862e0a7f255bfcacb595
Valère Monseur <dev@vale.re>
2021-09-12 20:56:17 +0200

    remove bottom margin when the category is collapsed

--- application/templates/collapse.js
+++ application/templates/collapse.js
@@ -1,9 +1,11 @@
 
 $('.collapse').not('.navbar-collapse').on('hidden.bs.collapse', function () {
+    $(this).closest('table').addClass('mb-0');
     window.sessionStorage.setItem(this.id + '@' + window.location.pathname, 'hidden');
 });
 
 $('.collapse').not('.navbar-collapse').on('shown.bs.collapse', function () {
+    $(this).closest('table').removeClass('mb-0');
     window.sessionStorage.removeItem(this.id + '@' + window.location.pathname);
 });
 
@@ -11,6 +13,7 @@ $(function () {
     $('.collapse').not('.navbar-collapse').each(function () {
         if (window.sessionStorage.getItem(this.id + '@' + window.location.pathname) !== null) {
             $(this).collapse();
+            $(this).closest('table').addClass('mb-0');
         }
     });
 });
