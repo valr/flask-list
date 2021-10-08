b7c6ee1046874f90b4931fa98d7015b4d98b9548
Valère Monseur <dev@vale.re>
2021-10-08 21:44:42 +0200

    move .ready function on top and use camel case for scrollY

--- application/templates/scroll.js
+++ application/templates/scroll.js
@@ -1,11 +1,11 @@
 
-$(window).on('beforeunload', function () {
-    window.sessionStorage.setItem('scrollY@' + window.location.pathname, window.scrollY);
+$(function () {
+    var scrollY = window.sessionStorage.getItem('scrollY@' + window.location.pathname);
+    if (scrollY !== null) {
+        window.scrollTo(0, scrollY);
+    }
 });
 
-$(function () {
-    var scrolly = window.sessionStorage.getItem('scrollY@' + window.location.pathname);
-    if (scrolly !== null) {
-        window.scrollTo(0, scrolly);
-    }
+$(window).on('beforeunload', function () {
+    window.sessionStorage.setItem('scrollY@' + window.location.pathname, window.scrollY);
 });
