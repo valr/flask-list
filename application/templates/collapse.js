c41dcca0678cd356bc3fa6a8dbe89afe4049cd3a
Valère Monseur <dev@vale.re>
2021-10-08 21:51:48 +0200

    move .ready function on top

--- application/templates/collapse.js
+++ application/templates/collapse.js
@@ -1,4 +1,13 @@
 
+$(function () {
+    $('.collapse').not('.navbar-collapse').each(function () {
+        if (window.sessionStorage.getItem(this.id + '@' + window.location.pathname) !== null) {
+            $(this).collapse();
+            $(this).closest('table').addClass('mb-0');
+        }
+    });
+});
+
 $('.collapse').not('.navbar-collapse').on('hidden.bs.collapse', function () {
     $(this).closest('table').addClass('mb-0');
     window.sessionStorage.setItem(this.id + '@' + window.location.pathname, 'hidden');
@@ -7,13 +16,4 @@ $('.collapse').not('.navbar-collapse').on('hidden.bs.collapse', function () {
 $('.collapse').not('.navbar-collapse').on('shown.bs.collapse', function () {
     $(this).closest('table').removeClass('mb-0');
     window.sessionStorage.removeItem(this.id + '@' + window.location.pathname);
-});
-
-$(function () {
-    $('.collapse').not('.navbar-collapse').each(function () {
-        if (window.sessionStorage.getItem(this.id + '@' + window.location.pathname) !== null) {
-            $(this).collapse();
-            $(this).closest('table').addClass('mb-0');
-        }
-    });
 });
