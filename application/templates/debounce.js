c55cd2ae4ab33c406e71b3257ac0ee707a06cbc7
Valère Monseur <dev@vale.re>
2021-10-13 22:19:54 +0200

    add missing debounce js function

---
+++ application/templates/debounce.js
@@ -0,0 +1,10 @@
+
+var debounce = (function () {
+    var timer = {};
+    return function (id, callback, delay, arg) {
+        if (timer[id]) {
+            clearTimeout(timer[id]);
+        }
+        timer[id] = setTimeout(callback, delay, arg);
+    };
+})();
