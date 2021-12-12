cc17308a9fe75d7a8a3c77bb4f3dbdeeb458e280
Valère Monseur <dev@vale.re>
2021-12-12 22:25:52 +0100

    fix the collapse feature following the review of the layout

--- application/templates/collapse.js
+++ application/templates/collapse.js
@@ -1,19 +1,39 @@
 
 $(function () {
     $('.collapse').not('.navbar-collapse').each(function () {
-        if (window.sessionStorage.getItem(this.id + '@' + window.location.pathname) !== null) {
-            $(this).collapse();
-            $(this).closest('table').addClass('mb-0');
+        var element = $(this);
+
+        if (window.sessionStorage.getItem(
+            $(element).attr('id') + '@' + window.location.pathname) !== null) {
+            $(element).collapse();
+            $(element)
+                .closest('table').parent()
+                .nextAll(':has(table):first').children('table:first')
+                .removeClass('mt-3').addClass('mt-0');
         }
     });
 });
 
 $('.collapse').not('.navbar-collapse').on('hidden.bs.collapse', function () {
-    $(this).closest('table').addClass('mb-0');
-    window.sessionStorage.setItem(this.id + '@' + window.location.pathname, 'hidden');
+    var element = $(this);
+
+    window.sessionStorage.setItem(
+        $(element).attr('id') + '@' + window.location.pathname, 'hidden');
+
+    $(element)
+        .closest('table').parent()
+        .nextAll(':has(table):first').children('table:first')
+        .removeClass('mt-3').addClass('mt-0');
 });
 
 $('.collapse').not('.navbar-collapse').on('shown.bs.collapse', function () {
-    $(this).closest('table').removeClass('mb-0');
-    window.sessionStorage.removeItem(this.id + '@' + window.location.pathname);
+    var element = $(this);
+
+    window.sessionStorage.removeItem(
+        $(element).attr('id') + '@' + window.location.pathname);
+
+    $(element)
+        .closest('table').parent()
+        .nextAll(':has(table):first').children('table:first')
+        .removeClass('mt-0').addClass('mt-3');
 });
