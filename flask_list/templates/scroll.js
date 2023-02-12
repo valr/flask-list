e1488745815ad15b11199a314e919951cfc0c181
Valère Monseur <dev@vale.re>
2023-02-12 17:51:12 +0100

    reorder functions

--- flask_list/templates/scroll.js
+++ flask_list/templates/scroll.js
@@ -1,13 +1,13 @@
-$(window).on("beforeunload", function () {
-    window.sessionStorage.setItem(
-        "scrollY@" + window.location.pathname,
-        window.scrollY
-    );
-});
-
 $(function () {
     var scrollY = window.sessionStorage.getItem("scrollY@" + window.location.pathname);
     if (scrollY !== null) {
         window.scrollTo(0, scrollY);
     }
+});
+
+$(window).on("beforeunload", function () {
+    window.sessionStorage.setItem(
+        "scrollY@" + window.location.pathname,
+        window.scrollY
+    );
 });
