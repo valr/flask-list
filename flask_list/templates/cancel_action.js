d6680a7459e8816337504ab1cf0cb27cd2715816
Valère Monseur <dev@vale.re>
2023-02-21 22:07:52 +0100

    rename js files

--- flask_list/templates/cancel_action.js
+++ flask_list/templates/cancel_action.js
@@ -1,6 +1,6 @@
 $("#cancel").click(function () {
-    // bypass the client side validations that still happen when
-    // the cancel button is clicked and a mandatory field is empty
+    /* bypass the client side validations that still happen when
+       the cancel button is clicked and a mandatory field is empty */
     window.location.href = "{{ cancel_url }}";
 });
 
